40c71d51b6d036e6667183879c5d4c0f669bab51
maulana <maulana.robert.mr@gmail.com>
2017-04-14 12:59:02 +0700

    add gitignore modified server

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+const index = require("./routes/index");
+
+app.use("/", index);
+
+app.listen(3000, () => {
+  console.log(`Server started!`);
+})
